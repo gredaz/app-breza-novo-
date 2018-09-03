e8d6514f814c25f835f26f48429602b1a23753b0
gredaz <pipogred@yahoo.com>
2018-09-03 10:35:52 +0200

    prvi commit

---
+++ src/main/webapp/app/entities/entity.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { BrezaArticleModule } from './article/article.module';
+import { BrezaCityModule } from './city/city.module';
+import { BrezaClientModule } from './client/client.module';
+import { BrezaTypeModule } from './type/type.module';
+import { BrezaPositionModule } from './position/position.module';
+import { BrezaEmployeeModule } from './employee/employee.module';
+/* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
+
+@NgModule({
+    // prettier-ignore
+    imports: [
+        BrezaArticleModule,
+        BrezaCityModule,
+        BrezaClientModule,
+        BrezaTypeModule,
+        BrezaPositionModule,
+        BrezaEmployeeModule,
+        /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
+    ],
+    declarations: [],
+    entryComponents: [],
+    providers: [],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class BrezaEntityModule {}
